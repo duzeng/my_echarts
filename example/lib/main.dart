2e5a931d8878af872602f922b07dbccfbe6728de
duzeng <duzengrass@163.com>
2019-10-04 15:58:59 +0800

    feat: update example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,64 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:my_echarts/echarts.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+void main() {
+  //debugPaintSizeEnabled = true;
+  runApp(ExampleApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+class ExampleApp extends StatefulWidget {
+  @override
+  _ExampleAppState createState() => _ExampleAppState();
+}
+
+class _ExampleAppState extends State<ExampleApp> {
+  Map<String, dynamic> _option = {};
 
   @override
   void initState() {
     super.initState();
+    var option = {
+      "xAxis": {
+        "type": 'category',
+        "data": ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      "yAxis": {"type": 'value'},
+      "series": [
+        {
+          "data": [820, 932, 901, 934, 1290, 1330, 1320],
+          "type": 'line',
+          "smooth": true
+        }
+      ]
+    };
+    setState(() {
+      _option = option;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Flutter Echarts Plugin Example'),
+          ),
+          body: Column(
+            children: <Widget>[
+              Expanded(
+                child: Echarts(data: _option),
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: RaisedButton(
+                      child: Text('Update'),
+                      onPressed: () {
+                        _update();
+                      },
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 5,
+              )
+            ],
+          )),
+    );
+  }
+
+  _update() {
     var option = {
-      "title": {
-        "text":"Echarts entry example"
-      },
+      "title": {"text": "Echarts entry example"},
       "tooltip": {},
       "lengend": {
         "data": ["Sales"]
       },
       "xAxis": {
-        "data": ["shirt","cardign","chiffon shirt","pants","heels","socks"]
+        "data": ["shirt", "cardign", "chiffon shirt", "pants", "heels", "socks"]
       },
       "yAxis": {},
       "series": [
         {
-          "name":"Sales",
+          "name": "Sales",
           "data": [5, 20, 36, 10, 10, 20],
           "type": "bar"
         }
       ]
     };
 
-    return new MaterialApp(
-      home: new Scaffold(
-          appBar: new AppBar(
-            title: const Text('Flutter Echarts Plugin Example'),
-          ),
-          body: new SingleChildScrollView(
-            child: new Column(
-              children: <Widget>[
-                //Expanded(child: Echarts( data: option),)
-                new Container(
-                  child: Echarts(height: 300, data: option),
-                  height: 300.0,
-                  width: 500.0,
-                ),
-              ],
-            ),
-          )),
-    );
+    setState(() {
+      _option = option;
+    });
   }
 }
